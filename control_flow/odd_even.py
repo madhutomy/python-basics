f536681eb0119966998a5a2acf1d543dd1ea8c1c
Madhu Tomy <madhutomy@inflexionpoint.io>
2024-06-13 22:45:37 -0500

    odd or even numbers with control flow statements

---
+++ control_flow/odd_even.py
@@ -0,0 +1,7 @@
+# Write a program to figure out if a number is odd or even
+number = input("Enter a number: ")
+if int(number) % 2 == 0:
+    print(f"{number} is even")
+else:
+    print(f"{number} is odd")
+
