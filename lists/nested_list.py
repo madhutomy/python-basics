9669e6ee1b7472406e28a716eb2bbc2ad0b694dc
Madhu Tomy <madhutomy@inflexionpoint.io>
2024-06-15 00:06:17 -0500

    nested lists example

---
+++ lists/nested_list.py
@@ -0,0 +1,16 @@
+fruits = ["banana",  "apple", "orange"]
+
+vegetables = ["beans", "tomato", "cauliflower"]
+
+eatables = [fruits, vegetables]
+
+print(eatables)
+print(eatables[0])
+print(eatables[1])
+print(eatables[1][1])
+print(eatables[0][0])
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+fruits[-1] = "Melons"
+fruits.append("Lemons")
+print(fruits)
