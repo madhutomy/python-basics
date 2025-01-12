424fd9fa945deec8558f2d4193ed3ff0a3682004
Madhu Tomy <madhutomy@inflexionpoint.io>
2025-01-11 19:10:35 -0600

    slices and tuples

---
+++ lists/slices.py
@@ -0,0 +1,5 @@
+cars = ["Toyota", "Honda", "Nissan"]
+print(cars[1:2])
+print(cars[2:])
+copied_cars = cars[:]
+print(copied_cars)
---
+++ lists/tuples.py
@@ -0,0 +1,6 @@
+# Tuples are immutable lists
+# If you want to store data that don't change throughout the life of a program, then store them in tuples
+houston_coordinates = (29.7, 95.3)
+print(houston_coordinates)
+# even though you can’t modify a tuple, you can assign a new value to a variable that represents a tuple.
+houston_coordinates = (30, 96)
